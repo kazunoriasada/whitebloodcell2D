7e50b850671b942723bd69ed3daeb766063ab799
浅田和則 <kazu.july05@gmail.com>
2019-10-16 15:15:17 +0900

    EventTrigger

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -48,6 +48,62 @@ public class Player : MonoBehaviour
         this.mainCamera             = Camera.main;
         this.mainCameraTransform    = this.mainCamera.transform;
     }
+   public void start()
+   {
+       //Event TriggerコンポーネントでDrugイベントを取得
+       //https://qiita.com/takesuke/items/e3b314aa7fd9111bc17f
+       var eventTrigger       = GetComponent<EventTrigger>();
+            var beginDragEntry     = new EventTrigger.Entry();
+            //PointerDown = 範囲内でボタンを押す(タップ)した時
+            beginDragEntry.eventID = EventTriggerType.PointerDown;
+            
+            /* 
+            デリゲート = メソッドを参照するための型
+            前提 →デリゲートを作るためだけに、クラスメソッドかインスタンスメソッドを定義するのは面倒で読みづらい
+            匿名関数を使えばメソッドを定義せずともインラインに処理を書いてデリゲートを作る事ができる
+            匿名関数には2種類あってその1つがラムダ式
+            https://qiita.com/RyotaMurohoshi/items/740151bd772889cf07de
+
+            ラムダ式
+            匿名関数を使えばインラインでデリゲートの処理を記述し、デリゲートを生成できる
+            匿名関数の一つはラムダ式
+            もう一つの匿名関数である匿名メソッドよりも簡潔に記述することができる
+            C# 3.0以降であれば匿名メソッド式でなく、ラムダ式を使うべき
+
+            */
+
+            //AddListener = UnityAction系のデリゲートを引数にとる
+            beginDragEntry.callback.AddListener(data =>
+            {
+                //OnBeginDrag() = タッチ開始座標を取得
+                OnBeginDrag((PointerEventData)data);
+            });
+           
+            eventTrigger.triggers.Add(beginDragEntry);
+
+            //ドラッグ開始
+            var dragEntry     = new EventTrigger.Entry();
+            //EventTriggerType.Drag = PointerDown後に押しっぱなしのまま移動している時
+            dragEntry.eventID = EventTriggerType.Drag;
+            
+            dragEntry.callback.AddListener(data =>
+            {
+                //OnDrag() = 現在のタッチ座標 - タッチ開始座標でひっぱりのベクトルを算出
+                OnDrag((PointerEventData)data);
+            });
+            eventTrigger.triggers.Add(dragEntry);
+
+            var endDragEntry = new EventTrigger.Entry();
+            endDragEntry.eventID = EventTriggerType.EndDrag;
+            endDragEntry.callback.AddListener(data =>
+            {
+                //OnEndDrag () = ひっぱりのベクトルに応じてキャラのRigidbodyを操作
+                OnEndDrag((PointerEventData)data);
+            });
+
+            eventTrigger.triggers.Add(endDragEntry);
+            
+   }
     public void Update() 
     {
         //画面外に出たら最初から
@@ -66,7 +122,8 @@ public class Player : MonoBehaviour
         return position;
     }
 
-    public void OnMouseDown()
+    
+    /* public void OnMouseDown()
     {
         this.dragStart = this.GetMousePosition();
 
@@ -99,5 +156,6 @@ public class Player : MonoBehaviour
     {
         this.physics.AddForce(force, ForceMode2D.Impulse);
     }
+    */
 
 }
